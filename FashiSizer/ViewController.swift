0e2ba51c91918cb842cf1b0a00968217b1fc2fed
Erdem <erdemtas.et@icloud.com>
2021-05-30 19:45:24 +0300

    Initial Commit

---
+++ FashiSizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FashiSizer
+//
+//  Created by Erdem Tas on 30.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
